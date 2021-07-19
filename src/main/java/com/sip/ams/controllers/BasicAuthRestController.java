c12e870ef9cea1abb22563d556df945cd339a40f
Sameh-Git <sameh.trabelsii92@gmail.com>
2021-07-19 10:04:23 +0100

    add role + user

--- src/main/java/com/sip/ams/controllers/BasicAuthRestController.java
+++ src/main/java/com/sip/ams/controllers/BasicAuthRestController.java
@@ -1,6 +1,11 @@
 package com.sip.ams.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import com.sip.ams.entities.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import com.sip.ams.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.sip.ams.entities.AuthenticationBean;
@@ -8,8 +13,21 @@ import com.sip.ams.entities.AuthenticationBean;
 @CrossOrigin(origins = "*")
 @RestController
 public class BasicAuthRestController {
+	@Autowired
+	private UserService userService;
+
 	@GetMapping(path = "/basicauth")
-	public AuthenticationBean basicauth() {
-		return new AuthenticationBean("You are authenticated");
+	public User basicauth() {
+		// return "User Amine : Role Admin";
+		// return new User("ma@gmail.com","1234");
+		// return "Success";
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		return user;
 	}
-}
+	/*
+	 * public Authentication basicauth() { return new Authentication("Spring");
+	 * //return "Success"; }
+	 */
+} 
+
